9d49bcc374b1344df807db2582ba55a5a2bd7716
Andrés Losada <nedrocoli@gmail.com>
2019-12-10 23:01:19 -0500

    Plantillita

--- entrega3Proyecto.py
+++ entrega3Proyecto.py
@@ -10,3 +10,60 @@ import sys
 import os
 
 os.system("clear")
+
+numeroHabitantes = [33000000, 230000000, 44000000]
+costoObjetivo = [0.7, 1, 0.6, 0.2, 0.1]
+porcentajeSexo = [0.62, 0.38]
+porcentajeHablantes = [0.52, 0.16]
+porcentajeDentroDeNicho = [0.03, 0.013]
+costoUbicacion[20/1000, 28/1000]
+costoPorImpresion = [66/1000, 1/1000, 10/1000]
+
+#Habitantes por Pais con Facebook
+numeroHabitantes[1] = 33000000  #Colombia
+numeroHabitantes[2] = 230000000 #E.E.U.U.
+numeroHabitantes[3] = 44000000  #UK
+
+#Costo Objetivos
+costoObjetivo[1] = 0.7   #Engagement
+costoObjetivo[2] = 1   #Brand Awareness
+costoObjetivo[3] = 0.6 #Traffic
+costoObjetivo[4] = 0.2 #Lead Generation
+costoObjetivo[5] = 0.1   #Conversions
+
+#Porcentaje de Cada Sexo con Facebook
+porcentajeSexo[1] = 0.62
+porcentajeSexo[2] = 0.38
+
+#Porcentaje de Hablantes 
+porcentajeHablantes[1] = 0.52 #Ingles
+porcentajeHablantes[2] = 0.16 #Español
+
+#Porcentaje Dentro De Nicho
+porcentajeDentroDeNicho[1] = 0.03 #Viajero
+porcentajeDentroDeNicho[2] = 0.013 #High Tech
+
+#Costo por Ubicacion
+costoUbicacion[1] = 20/1000 #Facebook
+costoUbicacion[2] = 28/1000 #Instagram
+
+#Costo por Impresion en dolares
+costoPorImpresion[1] = 66/1000 #Colombia
+costoPorImpresion[2] = 1/1000 #E.E.U.U.
+costoPorImpresion[3] = 10/1000 #UK
+
+for nh in range(len(numeroHabitantes)):
+    numHabTemp = numeroHabitantes(nh)
+    for co in range(len(costoObjetivo)):
+        costObj = costoObjetivo(co)
+        for ps in range(len(porcentajeSexo)):
+            porSex = porcentajeSexo(ps)
+            for ph in range(len(porcentajeHablantes)):
+                porHab = porcentajeHablantes(ph)
+                for pn in range(len(porcentajeDentroDeNicho)):
+                    porNich = porcentajeDentroDeNicho(pn)
+                    for cu in range(len(costoUbicacion)):
+                        costUb = costoUbicacion(cu)
+                        for ci in range(len(costoPorImpresion)):
+                            costImp = costoPorImpresion(ci)
+                            
